5456137f3858b1a27a6dcc1cd06cae69286d041f
sheilabett <sheilahbett@gmail.com>
2023-10-12 09:06:11 +0300

    styling

---
+++ index.js
@@ -0,0 +1,30 @@
+// function getVehicle(vehicle){
+//     let vehicleDiv = document.createElement("div");
+//             vehicleDiv.innerHTML = 
+//             `h1>${vehicle.name}</h1>
+//             h1>${vehicle.code}</h1>`       
+
+// }
+// function getVehicles() {
+//     fetch("https://parallelum.com.br/fipe/api/v1/carros/marcas")
+//     .then(response => response.json())
+//     .then(vehicleData => vehicleData.forEach(vehicle => getVehicle(vehicle)));
+// }
+
+// function initializeApp() {
+//     getVehicles();
+// }
+
+// initializeApp();
+document.addEventListener("DOMContentLoaded",()=>{
+    fetch("https://api.adviceslip.com/advice")
+    .then(res => res.json())
+    .then(data => {
+        let vehicleDiv = document.getElementById("vehicles")
+        console.log(data)
+        vehicleDiv.innerHTML = 
+        `<h1>${data.slip.advice}</h1>
+        
+        `;
+    })
+})
