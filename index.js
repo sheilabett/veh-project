75cc82bf98d932b1d1b9e8e978a1157f108920e3
sheilabett <sheilahbett@gmail.com>
2023-10-12 22:20:21 +0300

    Comment added sucessfully

--- index.js
+++ index.js
@@ -16,7 +16,11 @@
 // }
 
 // initializeApp();
+
+
 document.addEventListener("DOMContentLoaded",()=>{
+    
+
     fetch("https://api.adviceslip.com/advice")
     .then(res => res.json())
     .then(data => {
@@ -27,7 +31,48 @@ document.addEventListener("DOMContentLoaded",()=>{
         
         `;
     })
+    const addCommentForm = document.querySelector("#addcomment");
+    addCommentForm.addEventListener("submit",(e) => {
+        e.preventDefault()
+        let inputVal = e.target.leavecomment.value
+        let comment = {
+            name: inputVal
+        }
+        if(inputVal.length < 2){
+            alert("Invalid comment")
+            return
+        }
+        fetch("http://localhost:3000/feedback",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(comment)
+        })
+        .then(res => res.json())
+        .then(comment => console.log(comment))
+
+    })
 })
-function loadComment(){
+
+    // function loadComments() {
+    //     fetch('http://localhost:3000/comments')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         data.forEach(commentData => {
+    //             const commentElement = createCommentElement(commentData);
+    //             commentList.appendChild(commentElement);
+    //         });
+    //     })
+    //     .catch(error => {
+    //         console.error('Error:', error);
+    //     });
+    // }
+
+  
     
-}
+
+
+
+
+
